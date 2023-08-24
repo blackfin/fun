a824d2f6521d9730de7da5966802cd63eb5a225d
rvm <vrepiev@gmail.com>
2023-08-24 12:57:40 +0600

    Add action name

--- fun.py
+++ fun.py
@@ -54,15 +54,6 @@ def main():
 
       s = requests.session()
 
-      #page = s.get('http://www.mzra.ru/')
-      
-
-      #soup = BeautifulSoup(page.content, 'html.parser')
-      #element = soup.find("script", re.compile(r'\'bitrix_sessid\':\'(\w+)\''))
-
-      #all_scripts = soup.find_all("script")
-      #print element
-
       s.proxies = {}
       s.proxies['http'] = 'socks5h://localhost:9050'
       s.proxies['https'] = 'socks5h://localhost:9050'
@@ -74,31 +65,27 @@ def main():
       example = """
       <html>
       <body>
-      <div class="vote-form-box-buttons vote-vote-footer">
-        <span class="vote-form-box-button vote-form-box-button-first">
-          <input name="vote" value="Голосовать" type="submit"></span>
-	      <span class="vote-form-box-button vote-form-box-button-last">
-		      <a name="show_result" href="/?VOTE_ID=2&amp;view_result=Y">Результат</a>
+      <div class="action-form-box-buttons action-action-footer">
+        <span class="action-form-box-button action-form-box-button-first">
+          <input name="action" value="Action Name" type="submit"></span>
+	      <span class="action-form-box-button action-form-box-button-last">
+		      <a name="show_result" href="/?ACTION_ID=2&amp;view_result=Y">Результат</a>
 	      </span>
       </div>
       </body>
       </html>
       """
 
-      mainPageTored = s.get('http://www.mzra.ru/')
+      mainPageTored = s.get('http://www.xyz.ru/')
 
       cookies = s.cookies.get_dict() # get cookie from response
       #print(cookies)
       #print(mainPageTored.text)
-      found = re.search('\'bitrix_sessid\':\'(\w+)\'', mainPageTored.text)
+      found = re.search('\'xyz_sessid\':\'(\w+)\'', mainPageTored.text)
       extSessid = found.group(0)
 
       if (len(found.group(1)) == len('e186d9d04542717392ce439d44d69c52')):
-        print("Md5 bitrix_sessid is valid %s extracted from %s." % (found.group(1), found.group(0)))
-
-
-      #span class='vote-form-box-button vote-form-box-button-first'
-      #<div class="vote-form-box-buttons vote-vote-footer">
+        print("Md5 xyz_sessid is valid %s extracted from %s." % (found.group(1), found.group(0)))
 
 
       headers = {
@@ -112,33 +99,29 @@ def main():
       headers['User-Agent'] = ua.rstrip("\n\r")
 
       data = {
-        'vote': 'Y',
-        'PUBLIC_VOTE_ID': '2',
-        'vote_radio_2': '8',
-        'vote': '%D0%93%D0%BE%D0%BB%D0%BE%D1%81%D0%BE%D0%B2%D0%B0%D1%82%D1%8C'
+        'result': 'Y',
+        'PUBLIC_ACTION_ID': '2',
+        'action_radio_2': '8',
+        'action': '%D0%93%D0%BE%D0%BB%D0%BE%D1%81%D0%BE%D0%B2%D0%B0%D1%82%D1%8C'
         }
       data['sessid'] = found.group(1)
 
       print(data)
-
-      #mainPageTored.content
-      #print(mainPageTored.content)
       tored = BeautifulSoup(mainPageTored.content, 'html.parser')
-      #element = soup.find("script", re.compile(r'\'bitrix_sessid\':\'(\w+)\''))
-      span = tored.find("span", class_="vote-form-box-button vote-form-box-button-first")
-      spans = tored.find_all('span', {'class' : 'vote-form-box-button vote-form-box-button-first'})
+      span = tored.find("span", class_="action-form-box-button action-form-box-button-first")
+      spans = tored.find_all('span', {'class' : 'action-form-box-button action-form-box-button-first'})
       #all_scripts = soup.find_all("script")
       if (spans):
         count += 1
-        print("The vote is actived %s. Total successed %s for time %s" % (spans, count, time.time() - start_time))
+        print("The action is actived %s. Total successed %s for time %s" % (spans, count, time.time() - start_time))
         repsonseFrom = s.post(
-          'http://www.mzra.ru/', 
+          'http://www.xyz.ru/', 
           headers=headers, 
           cookies=cookies, 
           data=data
         )
         print(repsonseFrom.status_code)
-      print("Total voted requests %s" % (count))
+      print("Total action requests %s" % (count))
       time.sleep(10)
 
 if __name__ == '__main__':
